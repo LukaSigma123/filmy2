0947898c1c8dde058eb9e4a3fcf2041162a4395a
Łukasz <dzamal1835@gmail.com>
2024-10-03 19:44:09 +0200

    dodaje funkcje ktora obsluguje wysylanie formularza

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,24 +2,15 @@ import "bootstrap/dist/css/bootstrap.css";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  function onSubmit(event) {
+    event.preventDefault();
+
+    const tytul = event.target.elements.tytul.value;
+    const rodzaj = event.target.elements.rodzaj.value;
+    console.log(`tytul: ${tytul}, rodzaj: ${rodzaj}`);
+  }
+
 }
 
 export default App;
